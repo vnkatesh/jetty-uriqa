2ec6bf910028fc7b3968d40ce9460a12fe019781
venkatesh88@gmail.com <venkatesh88@gmail.com@8466edc3-aa94-7f31-4c6a-0664fba3947d>
2010-05-06 14:46:58 +0000

    Ok. Now has clean code. Devoid of any threading code. Works Ok. Haven't tested it fully as of yet

--- trunkSecret/src/main/java/org/eclipse/jetty/uriqa/UriqaHandler.java
+++ trunkSecret/src/main/java/org/eclipse/jetty/uriqa/UriqaHandler.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.http.MimeTypes;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
@@ -17,7 +16,6 @@ public class UriqaHandler extends AbstractHandler {
 	//getServer().
 
 	public UriqaHandler() {
-
 	}
 
 	public void handle(String target, Request baseRequest,
@@ -28,66 +26,28 @@ public class UriqaHandler extends AbstractHandler {
 			UriqaRepoHandler.getDefault(baseURI);
 		}
 		System.out.println("****************Handler***************");
-		//System.out.println("baseURI: "+baseURI);
 		//System.out.println("getLocalHost(): "+baseRequest.getConnection().getEndPoint().getLocalHost());
 		//TODO use UriqaMethods ENUM Matching.
 		//TODO what about MSEARCH or something? inferencing or querying model??
 		//TODO MQUERY? -> should return the query element names/id's or something. -> which the browser asks back if required.
-		if(baseRequest.getMethod().equals(UriqaConstants.Methods.MGET))
+		if(baseRequest.getMethod().equals(UriqaConstants.Methods.MGET) || baseRequest.getMethod().equals(UriqaConstants.Methods.MPUT)
+				|| baseRequest.getMethod().equals(UriqaConstants.Methods.MDELETE))
 		{
-			System.out.println("MGETting.");
-			//System.out.println("and the HttpServletRequest getMethod() gives: "+request.getMethod());
-			//response.setStatus(HttpServletResponse.SC_OK);
-			//response.setContentType(MimeTypes.TEXT_XML);
-			//response.setContentType(MimeTypes.TEXT_PLAIN);
-			//response.getOutputStream().write(new String("hello!\n").getBytes());
-			UriqaRepoHandler.getDefault().handleMget2(baseRequest, request,response.getWriter());
-			//UriqaRepoHandler.getDefault().handleMget(request.getPathInfo().startsWith("/")?request.getPathInfo():"/"+request.getPathInfo(),response.getWriter());
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType(MimeTypes.TEXT_XML);
+			UriqaRepoHandler.getDefault().handleRequest(request,response.getWriter(), baseRequest.getMethod());
 			//UriqaRepoHandler.getDefault().printModeltoOutput();
 			//response should be content type of mime binary data or ascii-n3-notations.
-			//handleMget static call.
-			// Mget (object name, response??)
-			//TODO Create the Basic Handler via extending AbstractHandler.
-			//TODO Then, think of thread and implementation of runnable
-			//TODO then, think of repository
-			//TODO then, think of how they shall interact.
-
-
+			//TODO Repository in a filesystem??
 			//TODO Or is it baseRequest.setHandled()??
+			((Request)request).setHandled(true);
+			baseRequest.setHandled(true);
 			//TODO check after installing other Handlers also..
 		}
-//		if(baseRequest.getMethod().equals(UriqaConstants.Methods.MPUT))
-//		{
-//			System.out.println("MPUT");
-//			//there should be content body here
-//			//TODO How should the content body structure be like? And how shall I put it as the content? Inputstream?
-//			response.setStatus(HttpServletResponse.SC_OK);
-//			response.setContentType(MimeTypes.TEXT_XML);
-//			UriqaRepoHandler.getDefault().handleMput(request.getPathInfo().startsWith("/")?request.getPathInfo():"/"+request.getPathInfo(),request);
-//			//baseRequest.getReader()
-//			//request.getReader()
-//			//handleMput static call
-//			((Request)request).setHandled(true);
-//		}
-//		if(baseRequest.getMethod().equals(UriqaConstants.Methods.MDELETE))
-//		{
-//			System.out.println("MDELETE");
-//			response.setStatus(HttpServletResponse.SC_OK);
-//			response.setContentType(MimeTypes.TEXT_XML);
-//			UriqaRepoHandler.getDefault().handleMdelete(request.getPathInfo().startsWith("/")?request.getPathInfo():"/"+request.getPathInfo());
-//			//no content body
-//			//no response body
-//			//just code if fail or not -> check uriqa definition pages again.
-//			//handleMdelete static call
-//			((Request)request).setHandled(true);
-//			baseRequest.setHandled(true);
-//		}
-		//System.out.println()
-		//System.out.println("VENKATESH DEBUG: target: "+target+" baseMethod: "+baseRequest.getMethod());
-		//response.setContentType("text/html");
-		//response.setStatus(HttpServletResponse.SC_OK);
-		//response.getWriter().println("<h1>Hello</h1>");
-
+		//TODO How should the content body structure be like? And how shall I put it as the content? Inputstream?
+		//baseRequest.getReader()
+		//request.getReader()
+		//just code if fail or not -> check uriqa definition pages again.
 
 	}
 
--- trunkSecret/src/main/java/org/eclipse/jetty/uriqa/UriqaRepoHandler.java
+++ trunkSecret/src/main/java/org/eclipse/jetty/uriqa/UriqaRepoHandler.java
@@ -13,67 +13,34 @@ import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.concurrent.BlockingQueue;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.util.component.AbstractLifeCycle;
-import org.eclipse.jetty.util.log.Log;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 @SuppressWarnings("serial")
-public class UriqaRepoHandler extends AbstractLifeCycle implements Serializable/*, ThreadPool*/ {
+public class UriqaRepoHandler extends AbstractLifeCycle implements Serializable{
 
 	private static UriqaRepoHandler sharedInstance = null;
-	private BlockingQueue<Runnable> _jobQ;
-
-	//	private static class Singleton {
-	private final QueuedThreadPool __pool;
-	//		static
-	//		{
-	//			try{
-	//				System.out.println("starting SingleTon");
-	//				__pool.start();}
-	//			catch(Exception e){Log.warn(e); System.exit(1);}
-	//		}
-	//	}
 
 	//private ModelFactory modelfactory;
 	private static Model model = null;
-	private final String INITIAL_REPO="org/eclipse/jetty/uriqa/commonwikiparsertestrdf.rdf";
+	//private final String INITIAL_REPO="org/eclipse/jetty/uriqa/commonwikiparsertestrdf.rdf";
 	private String baseURI="http://localhost";
 
-	public UriqaRepoHandler() throws Exception {
-		__pool = new QueuedThreadPool(200);
-		__pool.setName("venkatesh");
-		this.start();
-		__pool.start();
-	}
-
 	public UriqaRepoHandler(String baseURI) throws Exception {
-		__pool = new QueuedThreadPool(200);
-		__pool.setName("venkatesh");
-		this.baseURI=baseURI;
+		if(baseURI != null)
+			this.baseURI=baseURI;
 		this.start();
-		__pool.start();
 	}
 
 	public synchronized static UriqaRepoHandler getDefault()
 	{
-		if (sharedInstance == null) {
-			try {
-				sharedInstance = new UriqaRepoHandler();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return sharedInstance;
+		return getDefault(null);
 	}
 
 	public synchronized static UriqaRepoHandler getDefault(String baseURI)
@@ -107,6 +74,7 @@ public class UriqaRepoHandler extends AbstractLifeCycle implements Serializable/
 
 	public void printModeltoOutput()
 	{
+		//TODO Language as a parameter and dynamic output.
 		model.write(System.out, UriqaConstants.Lang.TURTLE);
 	}
 
@@ -165,7 +133,6 @@ public class UriqaRepoHandler extends AbstractLifeCycle implements Serializable/
 		try {
 			model.read(new FileReader(file), baseURI);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -174,195 +141,41 @@ public class UriqaRepoHandler extends AbstractLifeCycle implements Serializable/
 		sharedInstance = null;
 	}
 
-	//	public boolean dispatch(Runnable job) {
-	//		// TODO Auto-generated method stub
-	//		return false;
-	//	}
-	//
-	//	public int getIdleThreads() {
-	//		// TODO Auto-generated method stub
-	//		return 0;
-	//	}
-	//
-	//	public int getThreads() {
-	//		// TODO Auto-generated method stub
-	//		return 0;
-	//	}
-	//
-	//	public boolean isLowOnThreads() {
-	//		// TODO Auto-generated method stub
-	//		return false;
-	//	}
-	//
-	//	public void join() throws InterruptedException {
-	//		// TODO Auto-generated method stub
-	//		
-	//	}
+	public void handleRequest(HttpServletRequest request, PrintWriter writer, String method) {
+		//TODO Better response code if the resource was not actually found.
+		String baseURIpath = baseURI+(request.getPathInfo().startsWith("/")?request.getPathInfo():"/"+request.getPathInfo());
+		if (method.equals(UriqaConstants.Methods.MGET))
+			doGet(baseURIpath, writer);
+		if (method.equals(UriqaConstants.Methods.MPUT))
+			doPut(baseURIpath, request);
+		if (method.equals(UriqaConstants.Methods.MDELETE))
+			doDelete(baseURIpath);
+	}
 
-//	//TODO WTF is synchronized?? How do I use it for threads?
-//	public void handleMget(String path, PrintWriter writer) {
-//		//TODO Better response code if the resource was not actually found.
-//		System.out.println("Requested resource: "+baseURI+path);
-//		try{
-//			//System.out.println("WTF");
-//			//Thread.sleep(5000);
-//			UriqaJob job=new UriqaJob(baseURI+path,null,writer,UriqaConstants.Methods.MGET);
-//			//System.out.println("WFT1.5");
-//			//Thread.sleep(5000);
-//			//System.out.println("WTF2");
-//			//TODO Draw and understand how this whole threads and synchronized things work.
-//			//Thread.sleep(5000);
-//			if (!/*Singleton.*/__pool.dispatch(job))
-//			{
-//				//System.out.println("WTF3");
-//				//Thread.sleep(5000);
-//				job.run();
-//				//System.out.println("WTF4");
-//				//Thread.sleep(5000);
-//				//System.out.println("WTF5");
-//			}
-//			//System.out.println("WTF6");
-//			//Thread.sleep(5000);
-//			//System.out.println("WTF7");
-//		}
-//		catch(Exception e)
-//		{
-//			Log.warn(e);
-//		}
-//		//doGet(baseURI+path, writer);
-//	}
-
-//	public static void doGet(String baseURIPath, PrintWriter writer)
-//	{
-//		Resource tempResource = model.getResource(baseURIPath);
-//		tempResource.getModel().write(writer,UriqaConstants.Lang.RDFXML);
-//		tempResource = null;
-//	}
-//
-//	public void handleMput(String path, HttpServletRequest request)
-//	{
-//		//TODO Better response code.
-//		System.out.println("Putting resource: "+baseURI+path);
-//		try{
-//			UriqaJob job=new UriqaJob(baseURI+path,request,null,UriqaConstants.Methods.MPUT);			//TODO Draw and understand how this whole threads and synchronized things work.
-//			if (!/*Singleton.*/__pool.dispatch(job))
-//				job.run();
-//		}
-//		catch(Exception e)
-//		{
-//			Log.warn(e);
-//		}
-//
-//	}
-//
-//	public static void doPut(String baseURIPath, HttpServletRequest request)
-//	{
-//		try {
-//			model.read(request.getInputStream(), baseURIPath );
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	public void handleMdelete(String path)
-//	{
-//		//TODO Better response code.
-//		System.out.println("Deleting resource: "+baseURI+path);
-//		try{
-//			UriqaJob job=new UriqaJob(baseURI+path,null,null,UriqaConstants.Methods.MDELETE);
-//			//TODO Draw and understand how this whole threads and synchronized things work.
-//			if (!/*Singleton.*/__pool.dispatch(job))
-//				job.run();
-//		}
-//		catch(Exception e)
-//		{
-//			Log.warn(e);
-//		}
-//	}
-//
-//	public static void doDelete(String baseURIpath)
-//	{
-//		Resource tempResource = model.getResource(baseURIpath);
-//		synchronized(model)
-//		{
-//			model.remove(tempResource.getModel());
-//		}
-//		tempResource = null;
-//	}
-
-//	public class UriqaJob implements Runnable {
-//
-//		private String baseURIpath = null;
-//		private HttpServletRequest request = null;
-//		private PrintWriter writer = null;
-//		private String method = null;
-//
-//		public UriqaJob (String baseURIpath, HttpServletRequest request, PrintWriter writer, String method)
-//		{
-//			this.baseURIpath = baseURIpath;
-//			this.request = request;
-//			this.writer = writer;
-//			this.method = method;
-//		}
-//
-//		public void run() {
-//			System.out.println("JOB RUNNING");
-//			if (method.equals(UriqaConstants.Methods.MGET))
-//				doGet(baseURIpath,writer);
-//			if (method.equals(UriqaConstants.Methods.MPUT))
-//				doPut(baseURIpath,request);
-//			if (method.equals(UriqaConstants.Methods.MDELETE))
-//				doDelete(baseURIpath);
-//		}
-//
-//	}
-	
-	public static void doGet2(Request baseRequest, HttpServletRequest request,
-			PrintWriter writer)
+	public static void doGet(String baseURIPath, PrintWriter writer)
 	{
-		System.out.println("DOGET2");
-		Resource tempResource = model.getResource(request.getPathInfo().startsWith("/")?request.getPathInfo():"/"+request.getPathInfo());
+		Resource tempResource = model.getResource(baseURIPath);
 		tempResource.getModel().write(writer,UriqaConstants.Lang.RDFXML);
-		baseRequest.setHandled(true);
-		((Request)request).setHandled(true);
 		tempResource = null;
 	}
 
-	public class UriqaJob2 implements Runnable {
-
-		private Request baseRequest = null;
-		private HttpServletRequest request = null;
-		private PrintWriter writer = null;
-		private String method = null;
-
-		public UriqaJob2(Request baseRequest, HttpServletRequest request,
-				PrintWriter writer, String method)
-		{
-			this.baseRequest = baseRequest;
-			this.request = request;
-			this.writer = writer;
-			this.method = method;
+	public static void doPut(String baseURIPath, HttpServletRequest request)
+	{
+		try {
+			model.read(request.getInputStream(), baseURIPath );
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
-		public void run() {
-			System.out.println("JOB RUNNING");
-			if (method.equals(UriqaConstants.Methods.MGET))
-				doGet2(baseRequest, request, writer);
-		}
-
 	}
-	
-	public void handleMget2(Request baseRequest, HttpServletRequest request,
-			PrintWriter writer) {
-//		try{
-			UriqaJob2 job=new UriqaJob2(baseRequest, request, writer,UriqaConstants.Methods.MGET);
-//			if (!__pool.dispatch(job))
-				job.run();
-//		}
-//		catch(Exception e)
-//		{
-//			Log.warn(e);
-//		}
+
+	public static void doDelete(String baseURIpath)
+	{
+		Resource tempResource = model.getResource(baseURIpath);
+		synchronized(model)
+		{
+			model.remove(tempResource.getModel());
+		}
+		tempResource = null;
 	}
 
 }
